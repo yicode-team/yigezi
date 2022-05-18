eb85d0981a0f607d57e8e05b645172b3a148a7c5
chensuiyi <chenbimo@gmail.com>
2022-05-18 18:18:53 +0800

    🎨 优化性能和结构

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,48 @@
-let gulp = require("gulp");
-let gulpSass = require("gulp-sass")(require("sass"));
-async function public_default() {
-    gulp.src("./src/yigezi.scss")
-        //
-        .pipe(
-            gulpSass({
-                // outputStyle: "compressed",
-                precision: 5,
-                sourceMap: true,
-            }).on("error", gulpSass.logError)
-        )
-        .pipe(gulp.dest("./docs"));
-    await Promise.resolve("");
+"use strict";
+const srcDir = "src";
+const buildDir = "dist";
+const gulp = require("gulp");
+const del = require("del");
+const gulpPostcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const gulpIf = require("gulp-if");
+const gulpRename = require("gulp-rename");
+const sass = require("sass");
+const gulpSass = require("gulp-sass")(sass);
+const env = process.env.RELEASE_TYPE || "dev";
+const config = {
+    // postcss参数
+    postcssParams: [],
+    // sass参数
+    sassParams: {
+        outputStyle: "compressed",
+    },
+    // src参数
+    srcParams: {
+        allowEmpty: true,
+    },
+};
+
+config.postcssParams.push(autoprefixer({}));
+
+// 清除任务
+async function task_clean() {
+    del(buildDir);
+    return Promise.resolve(true);
 }
-gulp.watch("./src/**/*.scss", gulp.series(public_default));
-exports.default = public_default;
+
+// css任务
+async function task_css() {
+    gulp.src(`${srcDir}/yigezi.scss`, config.srcParams)
+        //
+        .pipe(gulpSass(config.sassParams))
+        .pipe(gulpPostcss(config.postcssParams))
+        .pipe(gulpRename("yigezi.min.css"))
+        .pipe(gulp.dest(buildDir));
+    return Promise.resolve(true);
+}
+
+// 监听公共构建
+gulp.watch(`${srcDir}/*.scss`, task_css);
+
+exports.default = gulp.series(task_clean, task_css);
